5709ca99ad5ecda51427f6c892809bc9f1617f28
yujisatojr <yujisatojr@gmail.com>
2020-11-15 11:32:26 -1000

    add border to skills section

--- src/Contact.js
+++ src/Contact.js
@@ -30,10 +30,10 @@ function Contact() {
       <div className="momo"><img src={cat} onClick={shoot}/></div>
       <div className="info">
         <ul>
-          <li><a href="https://www.facebook.com/yujisatojr" target="_blank" className="fa fa-facebook"></a></li>
-          <li><a href="https://twitter.com/yujisatojr"  target="_blank" className="fa fa-twitter"></a></li>
-          <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" className="fa fa-linkedin"></a></li>
-          <li><a href="https://github.com/yujisatojr" target="_blank"><img src={github}/></a></li>
+          <li><a href="https://www.facebook.com/yujisatojr" target="_blank" rel="noopener" className="fa fa-facebook"></a></li>
+          <li><a href="https://twitter.com/yujisatojr"  target="_blank" rel="noopener" className="fa fa-twitter"></a></li>
+          <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noopener" className="fa fa-linkedin"></a></li>
+          <li><a href="https://github.com/yujisatojr" target="_blank" rel="noopener"><img src={github}/></a></li>
         </ul>
         <p>Follow me on social media!</p>
         <p>&copy;2020 Yuji Sato. Website handcrafted by me.</p>
