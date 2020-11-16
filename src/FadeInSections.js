04305f3718a5b13c7be474707d3874fa7bd760b2
yujisatojr <yujisatojr@gmail.com>
2020-11-15 16:34:46 -1000

    make fade-in effect run only once

--- src/FadeInSections.js
+++ src/FadeInSections.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
 function FadeInSection(props) {
-    const [isVisible, setVisible] = React.useState(true);
+    const [isVisible, setVisible] = React.useState(false);
     const domRef = React.useRef();
     React.useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible(entry.isIntersecting));
-      });
-      observer.observe(domRef.current);
-      return () => observer.unobserve(domRef.current);
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setVisible(entry.isIntersecting);
+                }
+            });
+        });
+        observer.observe(domRef.current);
+        return () => observer.unobserve(domRef.current);
     }, []);
     return (
-      <div
-        className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
-        ref={domRef}
-      >
-        {props.children}
-      </div>
+        <div
+            className={`fade-in-section ${isVisible ? 'is-visible' : ''}`}
+            ref={domRef}
+        >
+            {props.children}
+        </div>
     );
-  }
+}
 
   export default FadeInSection;
