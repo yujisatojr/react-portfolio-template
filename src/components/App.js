b75bab7a5ac13b23d3d5d2e43b354c1fcf307c6e
Yuji Sato <yujisatojr@gmail.com>
2021-06-01 00:36:08 -1000

    Make about componet responsive

--- src/components/App.js
+++ src/components/App.js
@@ -58,19 +58,19 @@ function App() {
 
   return  (
     <div>
-      <div className="nav-bar">
-        <nav>
-            <ul>
-              <li><a href="#about">About</a></li>
-              <li><a href="#skills">Skills</a></li>
-              <li><a href="#project">Projects</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-        </nav>
-      </div>
+      
       <div className="introduction" id="intro">
         <h1>Yuji Sato</h1>
-        <p>Developer + Analyst</p>
+        <div className="nav-bar">
+          <nav>
+              <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#skills">Skills</a></li>
+                <li><a href="#project">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+          </nav>
+        </div>
       </div>
     </div>
   );
