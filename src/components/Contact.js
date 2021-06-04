374c089cceb287dda986a60d8d9cdbbf1e0c412a
Yuji Sato <yujisatojr@gmail.com>
2021-06-04 01:37:30 -1000

    Add media queries to css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,16 +6,24 @@ import { faKaggle, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-ico
 function Contact() {
  
   return  (
-    <div className="contact" id="contact">
-      <div className="info">
+    <div className="getintouch" id="contact">
+      <div className="section">
+        <h2>... got an idea?</h2>
+        <h1>Let's create awesome things together!</h1>
+        <div className="line"></div>
+        <p>Feel free to reach me at yujisatojr@go.byuh.edu</p>
+        <div className="info">
         <ul>
-          <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></li>
-          <li><a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
-          <li><a href="https://www.kaggle.com/yujisato" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faKaggle} size="2x"/></a></li>
-        </ul>
-
-        <p>Follow me on social media!</p>
-        <p>Portfolio handcrafted by <a href="https://github.com/yujisatojr/yujisatojr.com">Yuji Sato</a> with ❤️</p>
+            <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></li>
+            <li><a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
+            <li><a href="https://www.kaggle.com/yujisato" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faKaggle} size="2x"/></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="contact">
+        
+          <p>Portfolio handcrafted by <a href="https://github.com/yujisatojr/portfolio">Yuji Sato</a> with ❤️</p>
+        
       </div>
     </div>
   );
