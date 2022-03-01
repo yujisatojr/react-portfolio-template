78da05bfad7dcfdf3ce0726ad7ed12aeb1ebe549
Yuji Sato <yujisatojr@gmail.com>
2022-02-28 17:49:14 -1000

    Update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,17 +8,15 @@ function About() {
           <div class="col-lg-7">
             <img
               class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
+              src="https://yuji-aws-bucket.s3.us-east-2.amazonaws.com/about_image.jpg"
               alt=""
+              width="100%"
             />
           </div>
           <div class="col-lg-5">
             <h1>About Me</h1>
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+              I am a self-motivated developer and passionate about utilizing technology to turn creative ideas into tangible business values. I believe that using the right digital tools can maximize our ability to serve others and enhance each user’s experience. I am also a committed life-long learner and eager to implement more efficient algorithms.
             </p>
           </div>
         </div>
--- src/components/top/App.js
+++ src/components/top/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './style/App.css'
-import myProfile from './assets/img/profile.jpeg';
-//import FadeInSection from './FadeInSections'
 import Chip from '@material-ui/core/Chip';
 import { Button } from '@material-ui/core';
 
@@ -23,7 +21,7 @@ function App() {
           <a href="https://yuji-aws-bucket.s3.us-east-2.amazonaws.com/Resume_2022.pdf" target="_blank" rel="noreferrer"><Button variant="contained" size="large">Resume 2022</Button></a>
         </div>
         <div className="image-wrapper">
-          <img src={myProfile} alt="myProfile" />
+          <img src="https://yuji-aws-bucket.s3.us-east-2.amazonaws.com/home_image.jpeg" alt="myProfile" />
         </div>
       </div>
     </div>
