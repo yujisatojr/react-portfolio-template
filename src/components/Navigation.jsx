71cae589d618f5e3e7b4c2914c82174c01eb7196
Yuji Sato <yujisatojr@gmail.com>
2022-03-03 09:12:20 -1000

    Add build

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,29 +6,29 @@ function Navigation() {
     <div className="navigation">
       <nav>
         <div className="nav-bar">
-            <ul>
-                <li className="nav-item">
-                <NavLink className="nav-link" to="/">
-                  Home
-                  <span className="sr-only">(current)</span>
-                </NavLink>
-                </li>
-                <li className="nav-item">
-                <NavLink className="nav-link" to="/about">
-                  About
-                </NavLink>
-                </li>
-                <li className="nav-item">
-                <NavLink className="nav-link" to="/cases">
-                  Work
-                </NavLink>
-                </li>
-                <li className="nav-item">
-                <NavLink className="nav-link" to="/contact">
-                  Contact
-                </NavLink>
-                </li>
-            </ul>
+          <ul>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+              <span className="sr-only">(current)</span>
+            </NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/about">
+              About
+            </NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/cases">
+              Work
+            </NavLink>
+            </li>
+            <li className="nav-item">
+            <NavLink className="nav-link" to="/contact">
+              Contact
+            </NavLink>
+            </li>
+          </ul>
         </div>
       </nav>
     </div>
