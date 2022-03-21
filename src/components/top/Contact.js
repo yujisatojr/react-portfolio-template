6bbcca1c1720887f5f650d3a15e8f31dfa838cac
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-21 22:33:25 +0100

    Change social media hrefs

--- src/components/top/Contact.js
+++ src/components/top/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style/contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faKaggle, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
   return  (
@@ -13,9 +13,8 @@ function Contact() {
         <p>Feel free to reach out to me on social media.</p>
         <div className="info">
           <ul>
-            <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></li>
-            <li><a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
-            <li><a href="https://www.kaggle.com/yujisato" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faKaggle} size="2x"/></a></li>
+            <li><a href="https://www.linkedin.com/in/ayoub-lahyaoui-890029223/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></li>
+            <li><a href="https://github.com/alahyaoui" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
           </ul>
         </div>
       </div>
