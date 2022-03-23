6dc7b34ea4a15be64d5cd88c0fec7e84379e2fcf
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-23 14:32:30 +0100

    Add 3 more skills

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -3,8 +3,8 @@ import './style/skill.css';
 
 import Chip from '@material-ui/core/Chip';
 
-import { FaJava, FaJsSquare, FaHtml5, FaCss3, FaPhp, FaDatabase, FaPython, FaReact, FaLaravel, FaGitAlt } from "react-icons/fa";
-import { SiKotlin, SiSolidity, SiCplusplus, SiSpringboot, SiDjango, SiPostgresql, SiMongodb, SiHeroku, SiGnubash, SiPostman, SiVirtualbox, SiAndroidstudio, SiCisco } from "react-icons/si";
+import { FaJava, FaJsSquare, FaHtml5, FaCss3, FaPhp, FaDatabase, FaPython, FaReact, FaBootstrap, FaLaravel, FaGitAlt } from "react-icons/fa";
+import { SiKotlin, SiSolidity, SiCplusplus, SiSpringboot, SiDjango, SiPostgresql, SiSqlite, SiMongodb, SiHeroku, SiGnubash, SiPostman, SiVirtualbox, SiQt, SiAndroidstudio, SiCisco } from "react-icons/si";
 import { GrMysql, GrOracle, } from "react-icons/gr";
 import { DiAws } from "react-icons/di";
 import { CgShapeCircle } from "react-icons/cg";
@@ -36,6 +36,7 @@ function Skill() {
                         <h2>Frameworks</h2>
                         <p className="flex-chips">
                             <Chip className="zoom" avatar={<FaReact />} variant="outlined" label="React.js" />
+                            <Chip className="zoom" avatar={<FaBootstrap />} variant="outlined" label="Bootstrap" />
                             <Chip className="zoom" avatar={<SiSpringboot />} variant="outlined" label="Spring Boot" />
                             <Chip className="zoom" avatar={<FaLaravel />} variant="outlined" label="Laravel" />
                             <Chip className="zoom" avatar={<SiDjango />} variant="outlined" label="Django" />
@@ -48,6 +49,7 @@ function Skill() {
                         <p className="flex-chips">
                             <Chip className="zoom" avatar={<GrMysql />} variant="outlined" label="MySQL" />
                             <Chip className="zoom" avatar={<SiPostgresql />} variant="outlined" label="PostgreSql" />
+                            <Chip className="zoom" avatar={<SiSqlite />} variant="outlined" label="SQLite" />
                             <Chip className="zoom" avatar={<GrOracle />} variant="outlined" label="Oracle Apex" />
                             <Chip className="zoom" avatar={<SiMongodb />} variant="outlined" label="MongoDB" />
                         </p>
@@ -69,6 +71,7 @@ function Skill() {
                             <Chip className="zoom" avatar={<SiGnubash />} variant="outlined" label="Bash" />
                             <Chip className="zoom" avatar={<GiGears />} variant="outlined" label="Makefile" />
                             <Chip className="zoom" avatar={<SiVirtualbox />} variant="outlined" label="Virtual Machines" />
+                            <Chip className="zoom" avatar={<SiQt />} variant="outlined" label="Qt Creator" />
                             <Chip className="zoom" avatar={<SiAndroidstudio />} variant="outlined" label="Android Studio" />
                             <Chip className="zoom" avatar={<SiCisco />} variant="outlined" label="Cisco Packet Tracer" />
                         </p>
