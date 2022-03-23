09b00a12b4a1cffe5e8fdb727699974392f93a3e
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-23 14:35:44 +0100

    Update specialties list

--- src/components/home/Specialty.js
+++ src/components/home/Specialty.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import './style/skill.css';
+import './style/specialty.css';
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faAppStore, faPython } from '@fortawesome/free-brands-svg-icons';
+import { FaJava, FaJsSquare, FaHtml5, FaCss3, FaPhp, FaPython, FaReact, FaBootstrap, FaLaravel, FaGitAlt } from "react-icons/fa";
+import { SiKotlin, SiCplusplus, SiSpringboot, SiDjango, SiPostgresql, SiSqlite, SiHeroku, SiPostman, SiQt, SiAndroidstudio } from "react-icons/si";
+import { GrMysql, } from "react-icons/gr";
+import { CgShapeCircle } from "react-icons/cg";
 import FadeInSection from '../fade/FadeInSections'
 import Chip from '@material-ui/core/Chip';
+import { faAppStore, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
 
-function Specialty () {
-  return  (
+function Specialty() {
+  return (
     <div className="container" id="specialties">
       <FadeInSection>
         <div className="specialties-container">
@@ -15,57 +20,50 @@ function Specialty () {
           <div className="specialties-grid">
 
             <div className="specialty">
-              <FontAwesomeIcon icon={faReact} size="4x"/>
+              <FontAwesomeIcon icon={faReact} size="4x" />
               <h3>Full-Stack Web Development</h3>
               <p>Previously, I worked as a DevOps engineer and built 5+ RESTful web applications. I am a power user of Ruby on Rails framework and the JavaScript libraries such as React, Vue, Node, Express and Handlebars.</p>
-              <p className="flex-chips">Tech stack: 
-                <Chip variant="outlined" label="JavaScript" />
-                <Chip variant="outlined" label="HTML" />
-                <Chip variant="outlined" label="CSS" />
-                <Chip variant="outlined" label="SASS" />
-                <Chip variant="outlined" label="LESS" />
-                <Chip variant="outlined" label="PHP" />
-                <Chip variant="outlined" label="PostgreSQL" />
-                <Chip variant="outlined" label="MySQL" />
-                <Chip variant="outlined" label="MongoDB" />
+              <p className="flex-chips">Tech stack:
+                <Chip className="zoom" avatar={<FaReact />} variant="outlined" label="React.js" />
+                <Chip className="zoom" avatar={<FaBootstrap />} variant="outlined" label="Bootstrap" />
+                <Chip className="zoom" avatar={<SiSpringboot />} variant="outlined" label="Spring Boot" />
+                <Chip className="zoom" avatar={<FaLaravel />} variant="outlined" label="Laravel" />
+                <Chip className="zoom" avatar={<SiDjango />} variant="outlined" label="Django" />
+                <Chip className="zoom" avatar={<FaJsSquare />} variant="outlined" label="JavaScript" />
+                <Chip className="zoom" avatar={<FaHtml5 />} variant="outlined" label="HTML" />
+                <Chip className="zoom" avatar={<FaCss3 />} variant="outlined" label="CSS" />
+                <Chip className="zoom" avatar={<FaPhp />} variant="outlined" label="PHP" />
+                <Chip className="zoom" avatar={<SiHeroku />} variant="outlined" label="Heroku" />
+                <Chip className="zoom" avatar={<SiPostgresql />} variant="outlined" label="PostgreSql" />
+                <Chip className="zoom" avatar={<GrMysql />} variant="outlined" label="MySQL" />
+                <Chip className="zoom" avatar={<SiPostman />} variant="outlined" label="Postman" />
               </p>
             </div>
 
             <div className="specialty">
-              <FontAwesomeIcon icon={faAppStore} size="4x"/>
+              <FontAwesomeIcon icon={faAppStore} size="4x" />
               <h3>Software Development</h3>
               <p>I enjoy being part of an agile team to design, build, and deploy cross-platform software. My experience in software development ranges from building mobile applications to 2D/3D video games.</p>
-              <p className="flex-chips">Tech stack: 
-                <Chip variant="outlined" label="Java" />
-                <Chip variant="outlined" label="C" />
-                <Chip variant="outlined" label="C++" />
-                <Chip variant="outlined" label="C#" />
-                <Chip variant="outlined" label="Swift" />
-                <Chip variant="outlined" label="Git" />
-                <Chip variant="outlined" label="Docker" />
-                <Chip variant="outlined" label="AWS" />
+              <p className="flex-chips">Tech stack:
+                <Chip className="zoom" avatar={<FaJava />} variant="outlined" label="Java" />
+                <Chip className="zoom" avatar={<SiKotlin />} variant="outlined" label="Kotlin" />
+                <Chip className="zoom" avatar={<SiAndroidstudio />} variant="outlined" label="Android Studio" />
+                <Chip className="zoom" avatar={<SiCplusplus />} variant="outlined" label="C/C++" />
+                <Chip className="zoom" avatar={<SiQt />} variant="outlined" label="Qt Creator" />
+                <Chip className="zoom" avatar={<FaGitAlt />} variant="outlined" label="Git" />
+                <Chip className="zoom" avatar={<SiSqlite />} variant="outlined" label="SQLite" />
               </p>
             </div>
 
             <div className="specialty">
-              <FontAwesomeIcon icon={faPython} size="4x"/>
-              <h3>Data Analysis & BI</h3>
+              <FontAwesomeIcon icon={faBuilding} size="4x" />
+              <h3>Enterprise Resource Planning (ERP) Development</h3>
               <p>I love to empower business through the utilization of ELT, data visualization, data warehousing, and automation. I have experience in analyzing corporate data for better decision making.</p>
-              <p className="flex-chips">Tech stack: 
-                <Chip variant="outlined" label="SQL" />
-                <Chip variant="outlined" label="Python" />
-                <Chip variant="outlined" label="Pandas" />
-                <Chip variant="outlined" label="Matplotlib" />
-                <Chip variant="outlined" label="Seaborn" />
-                <Chip variant="outlined" label="Sklearn" />
-                <Chip variant="outlined" label="R" />
-                <Chip variant="outlined" label="Snowflake" />
-                <Chip variant="outlined" label="Domo" />
-                <Chip variant="outlined" label="Tableau" />
-                <Chip variant="outlined" label="Tensorflow" />
+              <p className="flex-chips">Tech stack:
+                <Chip className="zoom" avatar={<CgShapeCircle />} variant="outlined" label="Odoo" />
+                <Chip className="zoom" avatar={<FaPython />} variant="outlined" label="Python" />
               </p>
             </div>
-
           </div>
         </div>
       </FadeInSection>
