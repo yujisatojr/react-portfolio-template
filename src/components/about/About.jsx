14b9aeeb9bbc48d1a0371906deb92de5ffa71113
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-23 11:59:40 +0100

    Change About page content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import "./style/about.css"
 
 function About() {
   return (
     <div className="about">
       <div class="items_wrapper">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="https://yuji-aws-bucket.s3.us-east-2.amazonaws.com/about_image.jpg"
-              alt=""
-              width="100%"
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1>About Me</h1>
-            <p>
-              I am a self-motivated developer and passionate about utilizing technology to turn creative ideas into tangible business value. I believe that using the right digital tools can maximize our ability to serve others and enhance each user’s experience. I am also a committed life-long learner and eager to implement more efficient algorithms.
-            </p>
-          </div>
+        <div>
+          <img
+            className="image-wrapper"
+            src="https://alahyaoui-website-bucket.s3.eu-west-3.amazonaws.com/about_page.jpeg"
+            alt="about_picture"
+            width="100%"
+          />
+        </div>
+        <div>
+          <h1>About Me</h1>
+          <p className="about_text">
+            Hello, my name is Ayoub Lahyaoui, and I'm a 20-year-old Belgian computer science student in my third year.<br />
+            I enjoy socializing, meeting new people, and learning about different cultures, which leads me to enjoy traveling.<br />
+            I have been fortunate to visit a few countries and hope to visit more in the future.<br />
+            As a computer and technology enthusiast, I've developed a wide range of skills in a variety of computer-related fields, making me a highly adaptable individual. <br />
+            I strive to make use of all of my abilities but also to acquire new ones in order to help me complete my work as efficiently as possible.
+          </p>
         </div>
       </div>
     </div>
