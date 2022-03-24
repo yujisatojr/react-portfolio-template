17e512103a8f581333ca71855ccced9c1b2b1d8c
AyoubLahyaoui <ayoub.lahyaoui@hotmail.com>
2022-03-24 10:08:54 +0100

    Change BrowserRouter to HashRouter in order to fix SPA github 404 error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.css';
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import {
   Navigation,
   Footer,
