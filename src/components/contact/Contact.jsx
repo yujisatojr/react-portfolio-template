115d7cb07dcf3ba752259b1936659906076ce68e
Lahyaoui Ayoub <Ayoub.Lahyaoui@hotmail.com>
2022-03-24 01:29:37 +0100

    add toast error message for submit button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Button } from '@material-ui/core';
 import "./style/contact.css"
+import { toast } from 'react-toastify';
 
 function Contact() {
+  function notYetImplemented(e) {
+    e.preventDefault();
+      toast.error('Submit button not yet implemented, sorry for the inconvenience 😥, you can contact me through linkedin in the meantime');
+  }
+
   return (
     <div className="contact_form">
       <div className="items_wrapper">
@@ -17,7 +23,7 @@ function Contact() {
           <label for="subject">Inquiry</label>
           <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
-          <Button variant="contained" size="large">Submit</Button>
+          <Button variant="contained" size="large" onClick={notYetImplemented}>Submit</Button>
         </form>
       </div>
     </div>
