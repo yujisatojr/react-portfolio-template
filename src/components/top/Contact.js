74b2d31fdf3dcbae2a58ca3c522104d788e0ed23
Yuji Sato <yujisatojr@gmail.com>
2022-05-27 14:25:21 -1000

    Remove unsed link from contact

--- src/components/top/Contact.js
+++ src/components/top/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style/contact.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMedium, faKaggle, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faMedium, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Contact() {
   return  (
@@ -16,7 +16,6 @@ function Contact() {
             <li><a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x"/></a></li>
             <li><a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} size="2x"/></a></li>
             <li><a href="https://medium.com/@yujisatojr" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faMedium} size="2x"/></a></li>
-            <li><a href="https://www.kaggle.com/yujisato" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faKaggle} size="2x"/></a></li>
           </ul>
         </div>
       </div>
