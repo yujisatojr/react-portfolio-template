b4baaee58f8ef91999c60f619f6303c08ac70bc5
Yuji Sato <yujisatojr@gmail.com>
2022-05-30 11:03:17 -1000

    Fix padding issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,20 @@ import { Button } from '@material-ui/core';
 
 function Contact() {
   return (
-    <div className="contact_form">
-      <div className="items_wrapper">
-        <h1>Contact Me</h1>
-        <form>
-          <label for="fname">Full Name</label>
-          <input type="text" id="fname" name="fullname" placeholder="Your name.."/>
+    <div className="items_wrapper">
+      <h1>Contact Me</h1>
+      <form>
+        <label for="fname">Full Name</label>
+        <input type="text" id="fname" name="fullname" placeholder="Your name.."/>
 
-          <label for="fname">Email/Phone Number</label>
-          <input type="text" id="fname" name="contact" placeholder="Your contact information.."/>
+        <label for="fname">Email/Phone Number</label>
+        <input type="text" id="fname" name="contact" placeholder="Your contact information.."/>
 
-          <label for="subject">Inquiry</label>
-          <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+        <label for="subject">Inquiry</label>
+        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
-          <Button variant="contained" size="large">Submit</Button>
-        </form>
-      </div>
+        <Button variant="contained" size="large">Submit</Button>
+      </form>
     </div>
   );
 }
