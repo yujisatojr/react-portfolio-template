f3a5258fb5802b7228b6db572cc7779c4166a8b3
Yuji Sato <yujisatojr@gmail.com>
2022-05-30 12:28:26 -1000

    Add a responsive hamburger menu

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 function About() {
   return (
-    <div className="about">
-      <div class="items_wrapper">
+    <div class="items_wrapper">
+      <div className="about">
+        <h1>About Me</h1>
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
             <img
@@ -14,7 +15,6 @@ function About() {
             />
           </div>
           <div class="col-lg-5">
-            <h1>About Me</h1>
             <p>
               I am a self-motivated developer and passionate about utilizing technology to turn creative ideas into tangible business value. I believe that using the right digital tools can maximize our ability to serve others and enhance each user’s experience. I am also a committed life-long learner and eager to implement more efficient algorithms.
             </p>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faToggleOff, faToggleOn } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  
   return (
-    <div className="navigation">
-      <nav>
-        <div className="nav-bar">
-          <ul>
-            <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Home
-              <span className="sr-only">(current)</span>
-            </NavLink>
-            </li>
-            <li className="nav-item">
-            <NavLink className="nav-link" to="/about">
-              About
-            </NavLink>
-            </li>
-            <li className="nav-item">
-            <NavLink className="nav-link" to="/blog">
-              Blog
-            </NavLink>
-            </li>
-            <li className="nav-item">
-            <NavLink className="nav-link" to="/contact">
-              Contact
-            </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    <nav className="navigation">
+      <div className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
+        <FontAwesomeIcon icon={isNavExpanded ? faToggleOn : faToggleOff} size="3x"/>
+      </div>
+      <div className={isNavExpanded ? "nav-bar expanded" : "nav-bar"}>
+        <ul>
+          <li className="nav-item">
+          <NavLink className="nav-link" onClick={() => {setIsNavExpanded(!isNavExpanded)}} to="/">
+            Home
+            <span className="sr-only">(current)</span>
+          </NavLink>
+          </li>
+          <li className="nav-item">
+          <NavLink className="nav-link" onClick={() => {setIsNavExpanded(!isNavExpanded)}} to="/about">
+            About
+          </NavLink>
+          </li>
+          <li className="nav-item">
+          <NavLink className="nav-link" onClick={() => {setIsNavExpanded(!isNavExpanded)}} to="/blog">
+            Blog
+          </NavLink>
+          </li>
+          <li className="nav-item">
+          <NavLink className="nav-link" onClick={() => {setIsNavExpanded(!isNavExpanded)}} to="/contact">
+            Contact
+          </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
